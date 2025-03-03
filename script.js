c7f9ed8bffcf98a5e25cfbc4caf10180bcff1099
kaylannesantos <kaylannemendesdossantos.com>
2025-03-02 21:49:50 -0300

    Update CSS

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx x');
+    menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
